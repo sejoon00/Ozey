1ad09b53840b5f1afef91f1bf39bef724538d6f1
sejoon_00 <parksejoon313@gmail.com>
2022-07-12 00:15:19 +0900

    start2

---
+++ src/test/java/com/Ozey/ozey/web/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Ozey.ozey.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
